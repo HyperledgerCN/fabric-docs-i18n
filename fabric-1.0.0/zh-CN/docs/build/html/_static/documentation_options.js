9b3752b770b781bef8c87ebe3d26ac93b5dbd98a
立冬 郭 <guolidong@chainnova.com>
2018-04-09 15:29:13 +0800

    pure fabric-1.1.0_zhCn

--- fabric-1.0.0/zh-CN/docs/build/html/_static/documentation_options.js
+++
@@ -1,9 +0,0 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: 'master',
-    LANGUAGE: 'None',
-    COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '.html',
-    HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
-};
